853203660eb7999959820f12444b2d182aae2156
xXBlack-DragonXx <60008527+xXBlack-DragonXx@users.noreply.github.com>
2020-01-18 20:51:57 +0530

    Add files via upload

--- Space Invasion Pygame.py
+++ Space Invasion Pygame.py
@@ -96,6 +96,198 @@ while n<(n+2):
 
 
     def title_screen(screen):
+        quit_btn = UIElement(
+            center_position=(400, 500),
+            font_size=30,
+            bg_rgb=BLUE,
+            text_rgb=WHITE,
+            text="NEXT",
+            action=GameState.QUIT,
+        )
+        restart_btn = UIElement(
+                center_position=(400, 100),
+                font_size=15,
+                bg_rgb=BLUE,
+                text_rgb=WHITE,
+                text="The year is 2060.",
+            )
+        restart4_btn = UIElement(
+                center_position=(400, 150),
+                font_size=15,
+                bg_rgb=BLUE,
+                text_rgb=WHITE,
+                text="Because of ozone hole depletion, the aliens have finally invaded planet Earth.",
+            )
+        restart1_btn = UIElement(
+                center_position=(400, 200),
+                font_size=15,
+                bg_rgb=BLUE,
+                text_rgb=WHITE,
+                text="They are coming in massive numbers,",
+            )
+        restart5_btn = UIElement(
+                center_position=(400, 250),
+                font_size=15,
+                bg_rgb=BLUE,
+                text_rgb=WHITE,
+                text="as they will no longer burn while entering the atmosphere.",
+            )
+        restart2_btn = UIElement(
+                center_position=(400, 300),
+                font_size=15,
+                bg_rgb=BLUE,
+                text_rgb=WHITE,
+                text="The humans including you, the player, is forced to fight the incoming hordes of invasion.",
+            )
+        restart3_btn = UIElement(
+                center_position=(400, 350),
+                font_size=15,
+                bg_rgb=BLUE,
+                text_rgb=WHITE,
+                text="Can you hold them off? Or is the Earth doomed…",
+            )
+
+            
+
+
+        buttons = [ quit_btn, restart_btn, restart1_btn, restart2_btn, restart3_btn, restart4_btn, restart5_btn, ]
+
+        while True:
+            mouse_up = False
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                    mouse_up = True
+            screen.fill(BLUE)
+
+            for button in buttons:
+                ui_action = button.update(pygame.mouse.get_pos(), mouse_up)
+                if ui_action is not None:
+                    return ui_action
+                button.draw(screen)
+
+            pygame.display.flip()
+
+            
+
+
+    def play_level(screen):
+        return_btn = UIElement(
+            center_position=(140, 570),
+            font_size=20,
+            bg_rgb=BLUE,
+            text_rgb=WHITE,
+            text="Return to main menu",
+            action=GameState.TITLE,
+        )
+
+        while True:
+            mouse_up = False
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                    mouse_up = True
+            screen.fill(BLUE)
+
+            ui_action = return_btn.update(pygame.mouse.get_pos(), mouse_up)
+            if ui_action is not None:
+                return ui_action
+            return_btn.draw(screen)
+
+            pygame.display.flip()
+
+
+    class GameState(Enum):
+        QUIT = -1
+        TITLE = 0
+        NEWGAME = 1
+
+
+    if __name__ == "__main__":
+        main()
+
+    def create_surface_with_text(text, font_size, text_rgb, bg_rgb):
+        """ Returns surface with text written on """
+        font = pygame.freetype.SysFont("Courier", font_size, bold=True)
+        surface, _ = font.render(text=text, fgcolor=text_rgb, bgcolor=bg_rgb)
+        return surface.convert_alpha()
+
+
+    class UIElement(Sprite):
+        """ An user interface element that can be added to a surface """
+
+        def __init__(self, center_position, text, font_size, bg_rgb, text_rgb, action=None):
+            """
+            Args:
+                center_position - tuple (x, y)
+                text - string of text to write
+                font_size - int
+                bg_rgb (background colour) - tuple (r, g, b)
+                text_rgb (text colour) - tuple (r, g, b)
+                action - the gamestate change associated with this button
+            """
+            self.mouse_over = False
+
+            default_image = create_surface_with_text(
+                text=text, font_size=font_size, text_rgb=text_rgb, bg_rgb=bg_rgb
+            )
+
+            highlighted_image = create_surface_with_text(
+                text=text, font_size=font_size * 1.2, text_rgb=text_rgb, bg_rgb=bg_rgb
+            )
+
+            self.images = [default_image, highlighted_image]
+
+            self.rects = [
+                default_image.get_rect(center=center_position),
+                highlighted_image.get_rect(center=center_position),
+            ]
+
+            self.action = action
+
+            super().__init__()
+
+        @property
+        def image(self):
+            return self.images[1] if self.mouse_over else self.images[0]
+
+        @property
+        def rect(self):
+            return self.rects[1] if self.mouse_over else self.rects[0]
+
+        def update(self, mouse_pos, mouse_up):
+            """ Updates the mouse_over variable and returns the button's
+                action value when clicked.
+            """
+            if self.rect.collidepoint(mouse_pos):
+                self.mouse_over = True
+                if mouse_up:
+                    return self.action
+            else:
+                self.mouse_over = False
+
+        def draw(self, surface):
+            """ Draws element onto a surface """
+            surface.blit(self.image, self.rect)
+
+
+    def main():
+        pygame.init()
+
+        screen = pygame.display.set_mode((800, 600))
+        game_state = GameState.TITLE
+
+        while True:
+            if game_state == GameState.TITLE:
+                game_state = title_screen(screen)
+
+            if game_state == GameState.NEWGAME:
+                game_state = play_level(screen)
+
+            if game_state == GameState.QUIT:
+                pygame.quit()
+                return
+
+
+    def title_screen(screen):
         start_btn = UIElement(
             center_position=(400, 500),
             font_size=30,
@@ -112,53 +304,12 @@ while n<(n+2):
             text="Start",
             action=GameState.QUIT,
         )
-        restart_btn = UIElement(
-            center_position=(400, 100),
-            font_size=15,
-            bg_rgb=BLUE,
-            text_rgb=WHITE,
-            text="The year is 2060.",
-        )
-        restart4_btn = UIElement(
-            center_position=(400, 150),
-            font_size=15,
-            bg_rgb=BLUE,
-            text_rgb=WHITE,
-            text="Because of ozone hole depletion, the aliens have finally invaded planet Earth.",
-        )
-        restart1_btn = UIElement(
-            center_position=(400, 200),
-            font_size=15,
-            bg_rgb=BLUE,
-            text_rgb=WHITE,
-            text="They are coming in massive numbers,",
-        )
-        restart5_btn = UIElement(
-            center_position=(400, 250),
-            font_size=15,
-            bg_rgb=BLUE,
-            text_rgb=WHITE,
-            text="as they will no longer burn while entering the atmosphere.",
-        )
-        restart2_btn = UIElement(
-            center_position=(400, 300),
-            font_size=15,
-            bg_rgb=BLUE,
-            text_rgb=WHITE,
-            text="The humans including you, the player, is forced to fight the incoming hordes of invasion.",
-        )
-        restart3_btn = UIElement(
-            center_position=(400, 350),
-            font_size=15,
-            bg_rgb=BLUE,
-            text_rgb=WHITE,
-            text="Can you hold them off? Or is the Earth doomed…",
-        )
+        
 
         
 
 
-        buttons = [start_btn, quit_btn, restart_btn, restart1_btn, restart2_btn, restart3_btn, restart4_btn, restart5_btn, ]
+        buttons = [start_btn, quit_btn]
 
         while True:
             mouse_up = False
